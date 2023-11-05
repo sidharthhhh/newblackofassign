89c3991bfe4ab6238159f904eb7216e5c7bb5a53
Sidharth Shukla <swayam1044@gmail.com>
2023-11-06 04:51:02 +0530

    save

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,6 +1,7 @@
 import type { Config } from 'tailwindcss'
 
 const config: Config = {
+  purge: ['./src/**/*.tsx'], 
   content: [
     './pages/**/*.{js,ts,jsx,tsx,mdx}',
     './components/**/*.{js,ts,jsx,tsx,mdx}',
